4e22fb3849785d0efc51c0e9104dcf9008616293
Sushmita <74228635+Sushmita-kan@users.noreply.github.com>
2021-02-13 01:32:40 +0530

    Add files via upload

---
+++ Sushmita.py
@@ -0,0 +1,28 @@
+
+print("My first Python program")
+print("Thank You SO Much!!!!")
+msg = "Hello Sushmita"
+print(msg)
+msg.lower() 
+a,b=10,20
+print(a+b)
+print(type(a))
+a = True
+print(type(a))
+for i in range(10):print(i)  
+print(x = 10) if a > b else print(20)
+c = 12.5
+print(type(c))
+d = "Sushmita"
+print(type(d))
+e = 'a'
+print(type(e))
+x = 10
+def f1():
+    print("Don't disturb others.. in the session:",x)
+f1()
+import math
+print(math.sqrt(16))
+from random import * 
+for i in range (10):
+    print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep="")
